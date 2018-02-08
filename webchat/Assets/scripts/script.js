b5069ccec578d74edb1fe7348662c83afef80db0
Felipe Reis <31100280+FJRREIS@users.noreply.github.com>
2018-02-08 15:30:58 -0200

    Add files via upload

--- webchat/Assets/scripts/script.js
+++ webchat/Assets/scripts/script.js
@@ -1,9 +1,34 @@
 function login() {
-    document.getElementById("login_container").innerHTML='<object type="text/html" data="chat.html" ></object>';
+    
+    resize_components()
+    
+    load_chat();
+
 }
 
 function load_chat() {
-    var field = 'login_container';
-    var divdest = document.getElementById(field);
 
-  }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var conteudo_chat = this.responseText;
+            document.getElementById('chat_container').innerHTML = conteudo_chat;
+        }
+    };
+
+    xhttp.open("GET", "http://127.0.0.1:8887/chat.html");
+    xhttp.send();
+}
+
+function resize_components(){
+    var headercont = document.getElementById("header_container");
+    headercont.id = 'chat_header_container';
+    var h_label = document.getElementById("h_title");
+    h_label.id = 'chat_title';
+    var logo = document.getElementById("logo");
+    logo.id = 'chat_logo';
+    var imglogo = document.getElementById("img-logo");
+    imglogo.id = 'chat_img-logo';
+    var bodycont = document.getElementById("login_container");
+    bodycont.id = 'chat_container';
+}
